3c9d8a436c72fb3a776706abbfd824af0b11f562
Maksim Shatskii <45439905+shylskii@users.noreply.github.com>
2021-03-09 22:36:55 +0300

    внесены правки к макету

--- src/js/callbackscript.js
+++ src/js/callbackscript.js
@@ -3,6 +3,7 @@ var popup = document.querySelector('.callback-popup')
 var callbackButton = document.querySelectorAll('.callback-button');
 var callbackButtonExit = document.querySelector('.callback-popup__exit');
 var blur1 = document.querySelector('.blur_hide')
+var body1 = document.querySelector('body')
 export function callbackClickHandler () {
   callbackButtonExit.addEventListener('click', function () {
     popup.classList.add('callback-hide');
@@ -10,11 +11,13 @@ export function callbackClickHandler () {
   for (let i= 0; i < callbackButton.length; i++) {
     callbackButton[i].addEventListener('click', function () {
       popup.classList.remove('callback-hide');
+      body1.classList.add('overflowHidden');
       console.log('show');
     });
   }
   blur1.addEventListener('click', function () {
     popup.classList.add('callback-hide');
+    body1.classList.remove('overflowHidden');
   });
 };
 
