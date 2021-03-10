4689a6f27f600d3ccb27004d0a77344b501723ae
Maksim Shatskii <45439905+shylskii@users.noreply.github.com>
2021-03-10 21:57:17 +0300

    правки шрифта и бургера

--- src/js/burgerscript.js
+++ src/js/burgerscript.js
@@ -2,10 +2,12 @@ var burgermenu = document.querySelector('.burger')
 var burgerButton = document.querySelector('.burgerButton');
 var burgerExitButton = document.querySelector('.burger-exit-button');
 var blur3 = document.querySelector('.blur_hide3')
+var body3 = document.querySelector('body')
 
 export function burgerClickHandler () {
   burgerButton.addEventListener('click', function () {
     burgermenu.classList.remove('burger-exit');
+    body3.classList.add('overflowHidden');
   });
   burgerExitButton.addEventListener('click', function () {
     burgermenu.classList.add('burger-exit');
@@ -16,5 +18,6 @@ export function burgerClickHandler () {
   };
   blur3.addEventListener('click', function () {
     burgermenu.classList.add('burger-exit');
+    body3.classList.remove('overflowHidden');
   });
 };
